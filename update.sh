31e4ac2f298ad0ca12b369ff2cfaf85601f57872
Antoine Augusti <AntoineAugusti@users.noreply.github.com>
2014-11-16 00:16:31 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
+# Update dependencies
+composer update
+
+# Pull the latest version of the documentation
 rm -rf public/docs/manual
 cd public/docs
 git clone https://github.com/TeenQuotes/api-documentation
@@ -6,3 +10,6 @@ mv api-documentation manual/1.0
 cd manual/1.0
 mv README.md introduction.md
 sed -i 's#https://github.com/TeenQuotes/api-documentation/blob/master##g' introduction.md
+
+# Clear cache
+php artisan cache:clear
