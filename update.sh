3792114cb10d731d7dba5eb22e1104c0180bf78d
Antoine Augusti <antoine@augusti.fr>
2014-11-16 09:40:58 +0100

    Rename manual to API

--- update.sh
+++ update.sh
@@ -2,12 +2,12 @@
 composer update
 
 # Pull the latest version of the documentation
-rm -rf public/docs/manual
+rm -rf public/docs/api
 cd public/docs
 git clone https://github.com/TeenQuotes/api-documentation
-mkdir manual
-mv api-documentation manual/1.0
-cd manual/1.0
+mkdir api
+mv api-documentation api/1.0
+cd api/1.0
 mv README.md introduction.md
 sed -i 's#https://github.com/TeenQuotes/api-documentation/blob/master##g' introduction.md
 
