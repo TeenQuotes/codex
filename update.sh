892cce33ee89d9c9374ed643f4f231849a79cfe4
Antoine Augusti <antoine@augusti.fr>
2014-12-05 17:06:18 +0000

    sed is working differently on OS X

--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@ cd api/1.0
 # The first file is introduction.md for Codex
 mv README.md introduction.md
 # Replace URLs
-find ./ -type f -exec sed -i '' 's#https://github.com/TeenQuotes/api-documentation/blob/master##g' *.md {} \;
+find ./ -type f -exec sed -i 's#https://github.com/TeenQuotes/api-documentation/blob/master##g' *.md {} \;
 
 # Clear cache
 cd ../../../../
