d5d79ee1234fc04e459fb3ad8c3756270c2b5857
Antoine Augusti <antoine@augusti.fr>
2014-12-07 15:20:51 +0100

    Pull documentation for deep links

--- update.sh
+++ update.sh
@@ -4,18 +4,29 @@ git pull
 # Update dependencies
 composer update
 
-# Pull the latest version of the documentation
+# Remove old directories
 rm -rf public/docs/api
+rm -rf public/docs/deep-links
+
+# Create directories
 cd public/docs
-git clone https://github.com/TeenQuotes/api-documentation
 mkdir api
-mv api-documentation api/1.0
+mkdir deep-links
+
+# Pull the latest version of the API
+git clone https://github.com/TeenQuotes/api-documentation tmp
+mv tmp api/1.0
 cd api/1.0
 # The first file is introduction.md for Codex
 mv README.md introduction.md
 # Replace URLs
 find ./ -type f -exec sed -i 's#https://github.com/TeenQuotes/api-documentation/blob/master##g' *.md {} \;
+cd ../..
+
+# Pull the latest version of the doc for deep links
+git clone https://github.com/TeenQuotes/deep-links tmp
+mv tmp deep-links/1.0
 
 # Clear cache
-cd ../../../../
+cd ../../
 php artisan cache:clear
