1cb6f55e14de698c9f9324649383f0ade5a6f337
Antoine Augusti <antoine@augusti.fr>
2015-08-05 20:08:24 +0100

    Update the "deployment" script

--- update.sh
+++ update.sh
@@ -4,29 +4,32 @@ git pull
 # Update dependencies
 composer update
 
+# Pull the latest version of the API
+git clone https://github.com/TeenQuotes/api-documentation tmp-api
+cd tmp-api
+# The first file is introduction.md for Codex
+mv README.md introduction.md
+# Replace URLs
+find ./ -type f -exec sed -i 's#https://github.com/TeenQuotes/api-documentation/blob/master##g' *.md {} \;
+cd ..
+
+# Pull the latest version of the doc for deep links
+git clone https://github.com/TeenQuotes/deep-links tmp-deep-links
+
 # Remove old directories
 rm -rf public/docs/api
 rm -rf public/docs/deep-links
 
 # Create directories
-cd public/docs
-mkdir api
-mkdir deep-links
+mkdir -p public/docs/api
+mkdir -p public/docs/deep-links
 
-# Pull the latest version of the API
-git clone https://github.com/TeenQuotes/api-documentation tmp
-mv tmp api/1.0
-cd api/1.0
-# The first file is introduction.md for Codex
-mv README.md introduction.md
-# Replace URLs
-find ./ -type f -exec sed -i 's#https://github.com/TeenQuotes/api-documentation/blob/master##g' *.md {} \;
-cd ../..
-
-# Pull the latest version of the doc for deep links
-git clone https://github.com/TeenQuotes/deep-links tmp
-mv tmp deep-links/1.0
+# Move directories
+mv tmp-api public/docs/api/1.0
+mv tmp-deep-links public/docs/deep-links/1.0
 
 # Clear cache
-cd ../../
 php artisan cache:clear
+
+rm -rf tmp-api
+rm -rf tmp-deep-links
